be0e61c2980b579b460b77e2832a79da5645e25e
StPluto <ibavov.ismail@gmail.com>
2023-05-11 17:34:16 +0300

    Additions

--- scrapper.py
+++ scrapper.py
@@ -2,25 +2,52 @@ import requests
 from bs4 import BeautifulSoup
 import re
 baseurl = "https://github.com"
-topic = "python"
-f1 = open('posts.txt', 'w', encoding="utf-8")
-f1.write('')
-f1.close()
-for i in range(0,50):
-    print(f"Страница: {i}")
-    page = str(i)
-    link = "https://github.com/topics/" + topic + "?o=desc&s=stars&page=" + page
-    print(link)
-    response = requests.get(link).text
-    soup = BeautifulSoup(response, 'lxml')
-    posts = soup.find_all('div', 'd-flex flex-justify-between flex-items-start flex-wrap gap-2 my-3')
-    f1 = open('posts.txt', 'a', encoding="utf-8")
-    for x in posts:
-        star = re.search(r'title=\"([0-9, ,])+\"', str(x))
-        star = re.sub(r'[title=[\"]', '', star[0])
-        repo = re.search(r'href=\"(\/.*\/.*)\"', str(x))
-        url = baseurl + repo[1]
-        name = re.sub(r'\/.*\/','',repo[1])
-        f1.write("Репозиторий: " + name + ", Звёзд - " + star + " Ссылка: " + url)
-        f1.write('\n')
-    f1.close()
+topic = input("Enter the topic (example: 'php', 'python' , 'html'):")
+pages = int(input("Enter number of pages: "))
+mode = int(input("Enter the mode (MostStars = 1, RecentlyUpdated = 2):"))
+def MostStars(pages):
+    for i in range(0,pages):
+        f = open('MostStars.txt', 'w', encoding="utf-8")
+        f.write('')
+        f.close()
+        print(f"Page: {i}")
+        page = str(i)
+        link = "https://github.com/topics/" + topic + "?o=desc&s=stars&page=" + page
+        response = requests.get(link).text
+        soup = BeautifulSoup(response, 'lxml')
+        posts = soup.find_all('div', 'd-flex flex-justify-between flex-items-start flex-wrap gap-2 my-3')
+        f = open('MostStars.txt', 'w', encoding="utf-8")
+        for x in posts:
+            star = re.search(r'title=\"([0-9, ,])+\"', str(x))
+            star = re.sub(r'[title=[\"]', '', star[0])
+            repo = re.search(r'href=\"(\/.*\/.*)\"', str(x))
+            url = baseurl + repo[1]
+            name = re.sub(r'\/.*\/','',repo[1])
+            f.write("Repository: " + name + ", Stars - " + star + " Link: " + url)
+            f.write('\n')
+        f.close()
+
+def RecentlyUpdated(pages):
+    for i in range(0,pages):
+        print(f"Page: {i}")
+        page = str(i)
+        link = "https://github.com/topics/" + topic + "?o=desc&s=updated&page=" + page
+        response = requests.get(link).text
+        soup = BeautifulSoup(response, 'lxml')
+        posts = soup.find_all('div', 'd-flex flex-justify-between flex-items-start flex-wrap gap-2 my-3')
+        f1 = open('RecentlyUpdated.txt', 'a', encoding="utf-8")
+        for x in posts:
+            star = re.search(r'title=\"([0-9, ,])+\"', str(x))
+            star = re.sub(r'[title=[\"]', '', star[0])
+            star = star.replace(',','')
+            repo = re.search(r'href=\"(\/.*\/.*)\"', str(x))
+            url = baseurl + repo[1]
+            name = re.sub(r'\/.*\/','',repo[1])
+            if int(star) > 100:
+                f1.write("Repository: " + name + ", Stars - " + star + " Link: " + url)
+                f1.write('\n')
+        f1.close()
+if mode == 1:
+    MostStars(pages)
+else:
+    RecentlyUpdated(pages)
