3c40e398aa1297bc70f086d7cbe824d60a8114dc
StPluto <ibavov.ismail@gmail.com>
2023-12-07 17:10:30 +0300

    some refactoring

--- scrapper.py
+++ scrapper.py
@@ -1,11 +1,12 @@
 import requests
+import random
+import json
 from bs4 import BeautifulSoup
 import re
 baseurl = "https://github.com"
-topic = input("Enter the topic ('php','python','html','utilities' ,'chrome' ,'windows' ,'go'):")
+topic = random.choice(json.load(open('topics.json','r')))
 pages = int(input("Enter number of pages: "))
-
-def RecentlyUpdated(pages):
+def RecentlyUpdated():
     open('RecentlyUpdated.html', 'w').close()
     for i in range(0,pages):
         print(f"Page: {i + 1}")
@@ -13,8 +14,11 @@ def RecentlyUpdated(pages):
         link = "https://github.com/topics/" + topic + "?o=desc&s=updated&page=" + page
         response = requests.get(link).text
         soup = BeautifulSoup(response, 'lxml')
+
         posts = soup.find_all('div', 'd-flex flex-justify-between flex-items-start flex-wrap gap-2 my-3')
-        f1 = open('RecentlyUpdated.html', 'a', encoding="utf-8")
+        ftest = open('test.html','w');
+        ftest.write(posts)
+        """ f1 = open('RecentlyUpdated.html', 'a', encoding="utf-8")
         for x in posts:
             star = re.search(r'title=\"([0-9, ,])+\"', str(x))
             star = re.sub(r'[title=[\"]', '', star[0])
@@ -25,5 +29,5 @@ def RecentlyUpdated(pages):
             if int(star) > 50:
                 f1.write("Repository: " + name + ", Stars - " + star + " Link: <a href=" + url +"> Ссылка </a><br>")
                 f1.write('\n')
-        f1.close()
-RecentlyUpdated(pages)
+        f1.close() """
+RecentlyUpdated()
